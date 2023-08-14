266698938b80d623ce78ec128445cdf908473cb5
Kgiruc <kgiruc76@gmail.com>
2023-08-14 19:43:40 +0200

    add styles to chat

--- twg_messeneger/screens/ChatScreen.tsx
+++ twg_messeneger/screens/ChatScreen.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import { View, Text, KeyboardAvoidingView } from 'react-native';
-import { Bubble, GiftedChat, IMessage, InputToolbar, Send } from 'react-native-gifted-chat';
+import { Bubble, BubbleProps, Composer, ComposerProps, GiftedChat, IMessage, InputToolbar, InputToolbarProps, Send, SendProps } from 'react-native-gifted-chat';
 import { useMutation, useQuery } from '@apollo/client';
 import { GET_MESSAGES, SEND_MESSAGE } from '../graphql/queries';
 import { Message } from '../types/Message';
-import { chatStyles } from './styles/chatStyles';
+import palette from '../styles/colors';
+import { fonts } from '../styles/fonts';
+import SendIcon from '../assets/send.svg';
 
 type ChatProps = {
   route: { params: { roomId: string, userName: string, userId: string } };
@@ -59,42 +61,95 @@ function ChatScreen({ route }: ChatProps) {
     return <Text>Error: {error.message}</Text>;
   }
 
+  const renderBubble = (props: BubbleProps<IMessage>) => (
+    <Bubble 
+    {...props}
+    wrapperStyle={{
+      left: { backgroundColor: palette.white.main, marginBottom: 12 },
+      right: { backgroundColor: palette.plum.light_md, marginBottom: 12 },
+    }}
+    textStyle={{
+      left: {...fonts.bodyText},
+      right: {
+        ...fonts.bodyText,
+        color: palette.white.main
+      }
+    }}
+    renderTime={() => null}
+    />
+  )
+
+  const renderSend = (props: SendProps<IMessage>) => (
+    <Send {...props} 
+      containerStyle={{
+        paddingRight: 16,
+        alignSelf: 'center' 
+      }}
+    >
+      <SendIcon />
+    </Send>
+  )
+
+  const renderComposer = (props: ComposerProps) => {
+    return (
+      <Composer
+        {...props}
+        textInputStyle={{
+          ...fonts.title_input,
+          paddingVertical: 8,
+          paddingHorizontal: 16,
+          backgroundColor: palette.white.main,
+          borderTopRightRadius: 12,
+          borderTopLeftRadius: 12,
+          borderBottomLeftRadius: 12,
+          marginHorizontal: 12,
+          flex: 1,
+          paddingTop: 16,
+          height: 41,
+          alignSelf: 'center'
+        }}
+        multiline
+      />
+    );
+  };
+
+  const renderInputToolbar = (props: InputToolbarProps<IMessage>) => {
+    return (
+      <InputToolbar
+        {...props}
+        containerStyle={{
+          backgroundColor: palette.blue.light_md,
+          borderTopLeftRadius: 12,
+          borderTopRightRadius: 12,
+          height: 73,
+
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      />
+    );
+  };
+
+
+
   return (
     <View style={{ flex: 1 }}>
-        <GiftedChat
-          messages={messages}
-          onSend={newMessages => onSend(newMessages)}
-          user={{
-            _id: userId,
-          }}
-          renderInputToolbar={(props: any) => (
-            <InputToolbar
-              {...props}
-              containerStyle={chatStyles.inputToolbar}
-              primaryStyle={{ alignItems: 'center' }}
-            />
-          )}
-          renderSend={(props: any) => (
-            <Send {...props} containerStyle={chatStyles.sendButton}>
-              <Text style={chatStyles.sendButtonText}>Send</Text>
-            </Send>
-          )}
-          renderBubble={(props: any) => (
-            <Bubble
-              {...props}
-              wrapperStyle={{
-                left: { backgroundColor: 'white' },
-                right: { backgroundColor: 'purple' },
-              }}
-              textStyle={{
-                left: { color: 'black' },
-                right: { color: 'white' },
-              }}
-            />
-          )}
-        />
+      <GiftedChat
+        messages={messages}
+        onSend={newMessages => onSend(newMessages)}
+        user={{
+          _id: userId,
+        }}
+        placeholder=''
+        renderBubble={renderBubble}
+        renderAvatar={null}
+        alwaysShowSend
+        renderSend={renderSend}
+        renderComposer={renderComposer}
+        renderInputToolbar={renderInputToolbar}
+      />
     </View>
   );
-};
+}
 
 export default ChatScreen;
